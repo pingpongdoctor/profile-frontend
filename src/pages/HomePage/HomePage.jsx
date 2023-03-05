7a6d2c01f3ebdad01c3b377a1979a9fcdf76e0ee
Simon Tran <thanhnhantran1501@gmail.com>
2023-03-04 22:42:58 -0800

    change content

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -340,11 +340,10 @@ export default function HomePage({
               >
                 <h2>Get to know me!</h2>
                 <p>
-                  I am a doctor and blockchain enthusiast who is passionate
-                  about transitioning into web development and loves creating
-                  full-stack single-page applications. I am constantly striving
-                  to refine and make my codes more dynamic, with a focus on
-                  improving the user experience of my websites.
+                  I am a full stack developer naturally curious to learn and
+                  grow in the software development industry. With the emerging
+                  urge to enhance user experience, I consequently desire to
+                  develop high-usability web applications.
                 </p>
                 <p>
                   There is a wealth of knowledge to be gained and shared in the
